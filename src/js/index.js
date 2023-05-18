78e783f1019db2e1beb1513b799d91868e62f500
Fereisalmeida <116765850+Fereisalmeida@users.noreply.github.com>
2023-05-18 17:01:20 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+alert("Olá, seja bem vindo!");
+
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body")
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao")
+
+botaoAlterarTema.addEventListener("click", () => {
+    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+
+    body.classList.toggle("modo-escuro");
+
+    if (modoEscuroEstaAtivo) {
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png");
+    } else {
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png")
+    }
+
+});
